a52fabc22f5d2d35fd05d3ae8f38758c2c0af01c
Wandile Mlangeni <wandilemlangeni3@gmail.com>
2025-01-03 08:52:19 +0200

    simply js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  const affirmation = document.getElementById("affirmation-input").value;
+
+  new Typewriter("#display-affirmation", {
+    strings: [affirmation],
+    autoStart: true,
+    delay: 100,
+    cursor: "|",
+  });
+}
+
+let affirmationFormElement = document.getElementById("affirmation-form");
+affirmationFormElement.addEventListener("submit", generateAffirmation);
